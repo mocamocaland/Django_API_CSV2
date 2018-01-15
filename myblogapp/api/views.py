f5fc09de891c529da3eb93c5dd925a33c939e3dd
mocamoca <notoya@cnctor.jp>
2018-01-15 15:15:25 +0900

    csvでなおかつリストで返すAPIにした。日付を/ に変更するよう修正を実施する

--- myblogapp/api/views.py
+++ myblogapp/api/views.py
@@ -1,10 +1,12 @@
-from django.http import JsonResponse
+import csv
+from django.http import HttpResponse
+# from django.http import JsonResponse
 
 from posts.models import (
     Post,
 )
 
-
+'''
 def posts_list(request):
     posts = [post.to_list() for post in Post.objects.order_by("id").all()]
     return JsonResponse(posts, safe=False)
@@ -14,10 +16,10 @@ def posts_list(request):
 def posts_list(request):
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="blog.csv"'
+    # response['Content-Disposition'] = 'attachment; filename="blog.csv"'
 
+    posts = [post.to_list() for post in Post.objects.order_by("id").all()]
     writer = csv.writer(response)
-    writer.writerow([request.title, request.published, request.image, request.body])
+    writer.writerow(posts)
 
     return response
-'''
